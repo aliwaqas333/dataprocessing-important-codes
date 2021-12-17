2e30af2c9b3c90af1fa4915c2d795e115c438b2b
Ali Waqas <aliwaqas@codeforpakistan.org>
2021-12-17 14:14:12 -0600

    Update miou_calc.py

--- miou_calc/miou_calc.py
+++ miou_calc/miou_calc.py
@@ -1,4 +1,4 @@
-from sklearn.metrics import jaccard_score
+from sklearn.metrics import jaccard_score, f1_score
 import numpy as np
 import os
 import cv2
@@ -11,22 +11,25 @@ def compute_iou(y_pred, y_true):
         jaccard = jaccard_score(y_pred.flatten(),y_true.flatten(), pos_label=label)
         jaccards.append(jaccard)
     # print(f'avg={np.mean(jaccards)}')
-    return np.mean(jaccards)
+        f1 = f1_score(y_true.flatten(), y_pred.flatten(), pos_label= label)
+    return np.mean(jaccards), np.mean(f1)
 
 if __name__ == "__main__":
     preds = os.listdir("./pred")
     gts = os.listdir("./true")
     miou = 0
+    f1 = 0
     print(f"len of pred = {len(preds)}, len of gts = {len(gts)}")
     
     for idx, image in enumerate(preds):
         pred = cv2.imread(f"./pred/{image}")
         gt = cv2.imread(f"./true/{gts[idx]}")
-        iou = compute_iou(pred, gt)
+        iou, f1 = compute_iou(pred, gt)
         miou += iou
+        f1 += f1
         print(f"{image}: {iou}")
     print("================")
-    print(f"MIOU: {miou/len(preds)}")
+    print(f"MIOU: {miou/len(preds)}, F1_Score: {f1/len(preds)}")
     print("================")
 
 
